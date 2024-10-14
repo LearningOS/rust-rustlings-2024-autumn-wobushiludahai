1584946f3b104b45686aca2c7d4f22d01e43e243
landsea <wobushiludahai@163.com>
2024-10-14 22:58:20 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -1,5 +1,5 @@
 // clippy3.rs
-// 
+//
 // Here's a couple more easy Clippy fixes, so you can see its utility.
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
@@ -10,22 +10,26 @@
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        my_option.unwrap();
+        println!("my_option is None!");
+        // my_option.unwrap();
     }
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3,
         -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    let my_empty_vec: Vec<i32> = Vec::with_capacity(0);
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+
+    std::mem::swap(&mut value_a, &mut value_b);
+
+    // value_a = value_b;
+    // value_b = value_a;
     println!("value a: {}; value b: {}", value_a, value_b);
 }
