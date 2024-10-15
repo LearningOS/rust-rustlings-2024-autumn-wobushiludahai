738d9b8dcb77aa7b4e0bef4d3f20ba2446fb5cd7
landsea <wobushiludahai@163.com>
2024-10-15 12:39:37 +0800

    upate

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -20,8 +20,15 @@ struct Foo {
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
     // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
     // simply reconstruct the box from that pointer.
-    let mut ret: Box<Foo> = unsafe { ??? };
-    todo!("The rest of the code goes here")
+    let mut ret: Box<Foo> = unsafe {
+        let raw_ptr = ptr as *mut Foo;
+        Box::from_raw(raw_ptr)
+    };
+
+    ret.a = 1;
+    ret.b = Some("hello".to_owned());
+
+    ret
 }
 
 #[cfg(test)]
